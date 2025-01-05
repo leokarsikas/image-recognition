ad8d71d0e0d6c2c75a1e804aca624340fac720b6
Leo Karsikas <leo.karsikas@live.com>
2025-01-05 21:25:05 +0100

    uploading prototype

---
+++ demo/prototype.py
@@ -0,0 +1,75 @@
+import torch
+import torchvision.transforms as transforms
+from torchvision import models
+import torch.nn as nn
+from PIL import Image
+import os 
+
+# Initialize the model
+model = models.densenet121(weights='IMAGENET1K_V1')
+model.classifier = nn.Linear(model.classifier.in_features, 7) # 7 classes in our dataset
+
+model_path=os.path.abspath(os.path.join("..", "trained_models" , "utandecay_cuda_10epoch.pth"))
+
+model.load_state_dict(torch.load(model_path, weights_only=True), strict=False)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")  # CUDA 
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")  # Apple GPU
+else:
+    device = torch.device("cpu")  # CPU
+
+model = model.to(device)
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+class_to_idx =  {
+    0: 'akiec',
+    1: 'bcc',
+    2: 'bkl',
+    3: 'df',
+    4: 'mel',
+    5: 'nv',
+    6: 'vasc'
+}
+
+def predict_image(model, img_path, transform):
+    
+    image = Image.open(img_path).convert("RGB")
+    image = transform(image)
+    image = image.to(device)
+    
+    model.eval()
+    
+    with torch.no_grad():
+        output = model(image.unsqueeze(0)) # AI prediction
+        _, predicted = torch.max(output, 1)  # Get the class with the highest probability
+    
+    # find predicted class with index to the class label
+    predicted_class = class_to_idx[predicted.item()]
+
+    result = ""
+
+    if(predicted_class == "bcc" or predicted_class == "mel"):
+        result = "Malignant"
+
+    elif(predicted_class == "akiec"):
+        result = "Potentially malignant"    
+    else:
+        result = "benign"    
+    
+    return predicted_class, result
+
+
+image_path = os.path.abspath(os.path.join("test_images" ,"melanoma1.jpg"))
+# Test image
+#img_path = "demo\test_images\ISIC_0469776.jpg"  # image path
+predicted_class, result = predict_image(model, image_path, transform)
+print("\n-------------------- RESULTS --------------------\n")
+print(f"The predicted class for the image is: {predicted_class}\nWhich indicates that it is: {result}\n")
+print("----------------- END OF RESULTS ----------------\n")
---
+++ trainingmodel.py
@@ -0,0 +1,112 @@
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+from torchvision import models
+import torch.nn as nn
+import torch.optim as optim
+import os
+from PIL import Image
+from tqdm import tqdm  # The progress bar
+
+# Dataset Structure 
+class SkinLesionDataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+        
+        self.classes = sorted(os.listdir(img_dir))  # Folder names equals to the classes
+        self.class_map = {}
+        for id, cls in enumerate(self.classes):
+            self.class_map[cls] = id
+   
+        # Store all image paths and their corresponding labels in an array
+        self.image_paths = []
+        for cls in self.classes:
+            cls_folder = os.path.join(img_dir, cls)
+            if os.path.isdir(cls_folder):
+                for img_name in os.listdir(cls_folder):
+                    img_path = os.path.join(cls_folder, img_name)
+                    self.image_paths.append((img_path, self.class_map[cls]))
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, id):
+        img_path, label = self.image_paths[id]
+        image = Image.open(img_path).convert("RGB")  # Open image in RGB mode
+        if self.transform:
+            image = self.transform(image)
+        return image, torch.tensor(label)
+
+# Transformations 
+transform = transforms.Compose([
+    transforms.Resize((244, 244)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+# Set the dataset path
+train_dir = os.path.abspath(os.path.join("base_dir" , "train_dir"))  # Replace with your training directory path
+val_dir = os.path.abspath(os.path.join("base_dir" , "val_dir"))      # Replace with your validation directory path      
+
+# assign the datasets
+train_dataset = SkinLesionDataset(train_dir, transform=transform)
+val_dataset = SkinLesionDataset(val_dir, transform=transform)
+# DataLoaders
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+# Model initialization
+model = models.densenet121(weights='IMAGENET1K_V1') # pretrained weights
+num_classes = len(train_dataset.classes)
+model.classifier = nn.Linear(model.classifier.in_features, num_classes)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")  # CUDA 
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")  # Apple GPU
+else:
+    device = torch.device("cpu")  # CPU
+
+model = model.to(device)
+
+# Loss function and Optimizer
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training time 
+for epoch in range(10):  # Number of epochs
+    model.train()
+    running_loss = 0.0
+    
+    # show a progress bar, UI - pleasure
+    with tqdm(train_loader, desc=f"Epoch {epoch + 1}", unit="batch") as progress_bar:
+        for inputs, labels in progress_bar:
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            optimizer.zero_grad() # resets the gradients of model parameters
+            outputs = model(inputs)  # AI prediction
+            loss = loss_function(outputs, labels)  # Compute loss
+            loss.backward()   # backpropogate prediction loss
+            optimizer.step()  # update the models paramters
+            
+            running_loss += loss.item()  # running loss
+
+    print(f"Epoch {epoch + 1}, Loss: {running_loss / len(train_loader):.4f}")
+
+# Evaluation time
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in val_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Test Accuracy after evaluation: {100 * correct / total:.2f}%")
+
+# Saving the model...
+torch.save(model.state_dict(), 'utandecay_cuda_10epoch.pth')
